48fc726f08f6d690d4bc49a69404b5144407ec61
Trey Yoder <treyyoder@gmail.com>
2020-04-20 00:35:22 -0400

    Adding includes to prevent the necessity of downloading them during the build; Fixed issue #2 and #3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-echo '127.0.0.1 content.quakejs.com' >> /etc/hosts
 
 cd /var/www/html
 
-sed -i "s/10.0.0.2/${SERVER}/g" index.html
+sed -i "s/quakejs/${SERVER}/g" index.html
 
-sed -i "s/8085/${HTTP_PORT}/g" index.html
+sed -i "s/${SERVER}:80/${SERVER}:${HTTP_PORT}/g" index.html
 
 /etc/init.d/apache2 start
 
